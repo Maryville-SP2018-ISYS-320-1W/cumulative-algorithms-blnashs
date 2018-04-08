1bfecd8cbc55bd30e251315aaaa3170b7310f57a
Barbara Nash <bnash1@live.maryville.edu>
2018-04-08 12:46:56 -0500

    string9

--- src/P6_DoubleProblems.java
+++ src/P6_DoubleProblems.java
@@ -1,16 +1,19 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s): Barbara Nash
+	Date: 4/8/18
 */
 
 public class P6_DoubleProblems {
 
 	public static void main(String[] args) {
 		double gpa = 3.2;
-		if( gpa * 3 == 9.6 ) {
+		if(gpa * 3 == 9.6 );
+		
+		 {
 			System.out.println( "You earned enough credts");
 		}
 	}
 
 }
+//missing ;
